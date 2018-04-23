7997bd1a507329b798a20dce5ff082ab5607c749
Patrick Mifsud <patrickmifsud@outlook.com>
2018-04-23 11:58:18 +1000

    Miniplistic Theme 2.0 update

--- js/darktheme.js
+++ js/darktheme.js
@@ -3,14 +3,14 @@ document.body.style.color = sessionStorage.getItem('cc');
 
 function theme() {	
 	if (sessionStorage.getItem('bg') === 'rgb(244, 244, 244)') {
-		sessionStorage.setItem('bg', 'rgb(31, 31, 31)');
+		sessionStorage.setItem('bg', 'rgb(35, 40, 42');
 		sessionStorage.setItem('cc', 'white');
 		
 	} else if (sessionStorage.getItem('bg') == null || undefined) {
-		sessionStorage.setItem('bg', 'rgb(31, 31, 31)');
+		sessionStorage.setItem('bg', 'rgb(35, 40, 42');
 		sessionStorage.setItem('cc', 'white ');
 		
-	} else if (sessionStorage.getItem('bg') === 'rgb(31, 31, 31)') {
+	} else if (sessionStorage.getItem('bg') === 'rgb(35, 40, 42') {
 		sessionStorage.setItem('bg', 'rgb(244, 244, 244)');
 		sessionStorage.setItem('cc', '#333');
 	}
